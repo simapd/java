7895d9e8143ac0d52afd50daeffebfc1a17a0955
iannrb <ian007953@gmail.com>
2025-06-03 00:17:13 -0300

    feat: add RiskAreasEntity and RiskAreasRepository for managing risk areas

---
+++ src/main/java/br/com/simapd/simapd/modules/riskAreas/RiskAreasEntity.java
@@ -0,0 +1,33 @@
+package br.com.simapd.simapd.modules.riskAreas;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "risk_area")
+public class RiskAreasEntity {
+
+  @Id
+  private String id;
+
+  @Column(name = "name", nullable = false)
+  @Size(max = 100, message = "Name must not exceed 100 characters")
+  private String name;
+
+  @Column(name = "description", nullable = true)
+  @Size(max = 500, message = "Description must not exceed 500 characters")
+  private String description;
+
+  @Column(name = "created_at", nullable = false)
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  private LocalDateTime createdAt;
+}
---
+++ src/main/java/br/com/simapd/simapd/modules/riskAreas/RiskAreasRepository.java
@@ -0,0 +1,7 @@
+package br.com.simapd.simapd.modules.riskAreas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RiskAreasRepository extends JpaRepository<RiskAreasEntity, String> {
+  
+}
