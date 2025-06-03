2acf22330d22d8a62e4b8b0f29a7f294bd27014d
iannrb <ian007953@gmail.com>
2025-06-03 00:17:17 -0300

    feat: add integration test for SimapdApplication context loading

---
+++ src/test/java/br/com/simapd/simapd/SimapdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.simapd.simapd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimapdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
